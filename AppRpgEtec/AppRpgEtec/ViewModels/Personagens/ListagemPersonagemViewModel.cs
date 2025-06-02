8a6bb6d38d8c6c61bca101674346145f41de6bc4
coletti <matheus.coletti08@gmail.com>
2025-06-02 08:49:13 -0300

    Aula 15

--- AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
+++ AppRpgEtec/AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AppRpgEtec.Models;
 using AppRpgEtec.Services.Personagens;
 
@@ -19,9 +20,12 @@ namespace AppRpgEtec.ViewModels.Personagens
             string token = Preferences.Get("UsuarioToken", string.Empty);
             pService = new PersonagemService(token);
             Personagens = new ObservableCollection<Personagem>();
-
             _ = ObterPersonagens();
+
+            NovoPersonagemCommand = new Command(async () => { await ExibirCadastroPersonagem(); });
         }
+
+        public ICommand NovoPersonagemCommand { get; }
 
         public async Task ObterPersonagens()
         {
@@ -33,6 +37,18 @@ namespace AppRpgEtec.ViewModels.Personagens
             catch (Exception ex) {
                 await Application.Current.MainPage
                     .DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+        public async Task ExibirCadastroPersonagem()
+        {
+            try
+            {
+                await Shell.Current.GoToAsync("cadPersonagemView");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
             }
         }
     }
